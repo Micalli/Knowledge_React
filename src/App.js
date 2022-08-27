c20d208a869d066a267a779063ccc23fb4cc2c32
Micalli <bruno.micalli@uol.com.br>
2022-08-27 19:28:49 -0300

    lista e state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Post from "./Post";
 
 export default function App() {
+  const [post, setPosts] = useState([
+    { id: Math.random(), title: "Tilte#01", subtitle: "Sub#01", likes: 20 },
+    { id: Math.random(), title: "Tilte#02", subtitle: "Sub#02", likes: 50 },
+    { id: Math.random(), title: "Tilte#03", subtitle: "Sub#03", likes: 10 },
+  ]);
+  function handleRefresh() {
+    setTimeout(() => {
+      setPosts((prevState) => [
+        ...prevState,
+        {
+          id: Math.random(),
+          title: `Tilte#0${prevState.length + 1}`,
+          subtitle: `Sub#0${prevState.length + 1}`,
+          likes: 51,
+        },
+      ]);
+    }, 2000);
+  }
   return (
     <>
       <Header title="Blog">
-        <h2>Subtitulo</h2>
+        <h2>
+          Post da semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
       <hr />
-      <Post
+      {post.map((value) => (
+        <Post
+          key={value.id}
+          likes={value.likes}
+          post={{
+            title: value.title,
+            subtitle: value.subtitle,
+          }}
+        />
+      ))}
+      {/* <Post
         likes={20}
         post={{
           title: "Titulo da noticia 1",
@@ -29,7 +60,7 @@ export default function App() {
           title: "Titulo da noticia 3",
           subtitle: "Subtitle da noticia 3",
         }}
-      />
+      /> */}
     </>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default function Header(props) {
   return (
@@ -8,3 +9,12 @@ export default function Header(props) {
     </>
   );
 }
+
+Header.PropTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+Header.defaultProps = {
+  title: ` Blog`,
+};
